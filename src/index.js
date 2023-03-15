2213691586747368ce207a49e26254a19b5ccd35
ezeur <ezeur@LAPTOP-G9FVKSTN>
2023-03-15 15:49:25 -0300

    Added a flat space with some tweaks

--- src/index.js
+++ src/index.js
@@ -9,19 +9,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement ); 
 
+//Setting the lights for our scene
+const ambient_light = new THREE.AmbientLight(0xbda355);
+const directional_light = new THREE.DirectionalLight(0xffffff, 1);
+ambient_light.add(directional_light);
+scene.add(ambient_light);
+
+//Setting up flat space of the metaverse
+const geometry_space = new THREE.BoxGeometry(100,0.2, 50);
+const material_space = new THREE.MeshPhongMaterial({color: 0xffffff});
+const space = new THREE.Mesh(geometry_space, material_space);
+scene.add(space);
+
+
 //Cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
 camera.position.z = 5;
 
-//Setting the lights for our scene
-const ambient_light = new THREE.AmbientLight(0x404040);
-const directional_light = new THREE.DirectionalLight(0x00ff00, 1);
-ambient_light.add(directional_light);
-scene.add(ambient_light);
+camera.position.set(10, 5, 40);
 
 function animate() {
     cube.rotation.x +=0.01;
